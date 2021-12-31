dbd55af129c02a917e8bee38483ba5a1e6808f3e
PKZOID <93982559+PKZOID@users.noreply.github.com>
2021-12-31 09:10:54 +0500

    Button Templete

---
+++ Button/templete.py
@@ -0,0 +1,162 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+from tkinter import filedialog
+import pyperclip
+
+__author__ = 'PKZOID/PKGAMER/ATIFSIDDIQUE'
+__copyright__ = 'Copyright (C) 2020, PKZOID'
+__credits__ = ['PKZOID']
+__license__ = 'The MIT License (MIT)'
+__version__ = '1.0'
+__maintainer__ = 'PKZOID'
+__email__ = 'Snipergamerpk@gmail.com'
+__status__ = 'Beta'
+
+_AppName_ = 'Material'
+
+templete = Tk()
+templete.resizable(False,False)
+templete.geometry('350x700')
+templete.title("Material")
+
+def BuymeCoffe():
+    pyperclip.copy("thumbialbutton1_photo = PhotoImage(file='BuymeCoffe.png')\n thumbialbutton1 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton1_photo,activebackground='#3D2B27',command=copy)\nthumbialbutton1.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def BuymeCoffe_1():
+    pyperclip.copy("thumbialbutton2_photo = PhotoImage(file='Buymecoffe button 1.png')\n thumbialbutton2 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton2_photo,activebackground='#3D2B27',command=copy)\nthumbialbutton2.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def thumbialbutton3():
+    pyperclip.copy("thumbialbutton3_photo = PhotoImage(file='Donate button.png')\n thumbialbutton3 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton3_photo,activebackground='#3D2B27',command=copy)\nthumbialbutton3.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def thumbialbutton4():
+    pyperclip.copy("thumbialbutton4_photo = PhotoImage(file='donate button 1.png')\n thumbialbutton4 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton4_photo,activebackground='#3D2B27',command=copy)\nthumbialbutton4.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def thumbialbutton5_photo():
+    pyperclip.copy("thumbialbutton5_photo = PhotoImage(file='donate button 3.png')\n thumbialbutton5 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton5_photo,activebackground='#3D2B27',command=copy)\nthumbialbutton5.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def thumbialbutton6():
+    pyperclip.copy("thumbialbutton6_photo = PhotoImage(file='donate button 2.png')\n thumbialbutton6 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton6_photo,activebackground='#3D2B27',command=copy)\nthumbialbutton6.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def line_photo1():
+    pyperclip.copy("line_photo1 = PhotoImage(file='Button Material.png')\n line_photo1 = Button(templete,bg='#3D2B27',border=0,image=line_photo1,activebackground='#3D2B27',command=copy)\nline_photo1.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def button_material1():
+    pyperclip.copy("button_material1 = PhotoImage(file='Button Material White.png')\n button_material1 = Button(templete,bg='#3D2B27',border=0,image=button_material1,activebackground='#3D2B27',command=copy)\button_material1.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def button_material():
+    pyperclip.copy("button_material = PhotoImage(file='Button Material Orange.png')\n button_material1 = Button(templete,bg='#3D2B27',border=0,image=button_material,activebackground='#3D2B27',command=copy)\button_material1.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def button_material3():
+    pyperclip.copy("button_material3 = PhotoImage(file='Button Material Orange1.png')\n button_material = Button(templete,bg='#3D2B27',border=0,image=button_material,activebackground='#3D2B27',command=copy)\button_material.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def button_material4():
+    pyperclip.copy("button_material3 = PhotoImage(file='Button Material Pink.png')\n button_material = Button(templete,bg='#3D2B27',border=0,image=button_material,activebackground='#3D2B27',command=copy)\button_material.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+def button_material5():
+    pyperclip.copy("button_material3 = PhotoImage(file='Button Material green.png')\n button_material = Button(templete,bg='#3D2B27',border=0,image=button_material,activebackground='#3D2B27',command=copy)\button_material.place(x=10,y=70")
+    spam = pyperclip.paste()
+    messagebox.showinfo("Copy","Code Has Been Copy")
+    messagebox.showinfo("Thanks For Using","Thanks For Using My App Coming Update With New Material Button")
+
+
+icon = PhotoImage(file='icon.dll')
+templete.iconphoto(False,icon)
+
+background = Label(templete,bg='#3D2B27',border=0,width=70,height=50)
+background.pack()
+
+title_label = Label(templete,text="Donate Button",foreground='white',font=('bold',13),bg='#3D2B27')
+title_label.place(x=100,y=45)
+
+donate_title = PhotoImage(file='line.png')
+donate_label = Label(templete,bg='#3D2B27',border=0,image=donate_title)
+donate_label.place(x=0,y=65)
+
+thumbialbutton1_photo = PhotoImage(file='BuymeCoffe.png')
+thumbialbutton1 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton1_photo,activebackground='#3D2B27')
+thumbialbutton1.place(x=10,y=70)
+
+thumbialbutton2_photo = PhotoImage(file='Buymecoffe button 1.png')
+thumbialbutton2 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton2_photo,activebackground='#3D2B27')
+thumbialbutton2.place(x=170,y=72)
+
+thumbialbutton3_photo = PhotoImage(file='Donate button.png')
+thumbialbutton3 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton3_photo,activebackground='#3D2B27')
+thumbialbutton3.place(x=15,y=120)
+
+thumbialbutton4_photo = PhotoImage(file='Donate button 1.png')
+thumbialbutton4 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton4_photo,activebackground='#3D2B27')
+thumbialbutton4.place(x=180,y=120)
+
+thumbialbutton5_photo = PhotoImage(file='donate button 2.png')
+thumbialbutton5 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton5_photo,activebackground='#3D2B27')
+thumbialbutton5.place(x=15,y=170)
+
+thumbialbutton6_photo = PhotoImage(file='Donate button 3.png')
+thumbialbutton6 = Button(templete,bg='#3D2B27',border=0,image=thumbialbutton6_photo,activebackground='#3D2B27')
+thumbialbutton6.place(x=180,y=170)
+
+line_photo1 = PhotoImage(file='line.png')
+line_label = Label(templete,bg='#3D2B27',border=0,image=line_photo1)
+line_label.place(x=0,y=235)
+
+title_label = Label(templete,text="Button Material",foreground='white',font=('bold',13),bg='#3D2B27')
+title_label.place(x=100,y=210)
+
+button_material1 = PhotoImage(file='Button Material.png')
+button_material = Button(templete,image=button_material1,border=0,bg='#3D2B27',activebackground='#3D2B27')
+button_material.place(x=15,y=240)
+
+button_material2 = PhotoImage(file='Button Material White.png')
+button_material3 = Button(templete,image=button_material2,border=0,bg='#3D2B27',activebackground='#3D2B27')
+button_material3.place(x=180,y=240)
+
+button_material_orange = PhotoImage(file='Button Material Orange.png')
+button_material4 = Button(templete,image=button_material_orange,border=0,bg='#3D2B27',activebackground='#3D2B27')
+button_material4.place(x=15,y=285)
+
+button_material_blue = PhotoImage(file='Button Material Orange1.png')
+button_material5 = Button(templete,image=button_material_blue,border=0,bg='#3D2B27',activebackground='#3D2B27')
+button_material5.place(x=180,y=285)
+
+button_material_pink = PhotoImage(file='Button Material Pink.png')
+button_material6 = Button(templete,image=button_material_pink,border=0,bg='#3D2B27',activebackground='#3D2B27')
+button_material6.place(x=15,y=330)
+
+button_material_green = PhotoImage(file='Button Material green.png')
+button_material7 = Button(templete,image=button_material_green,border=0,bg='#3D2B27',activebackground='#3D2B27')
+button_material7.place(x=180,y=330)
+
+line_photo2 = PhotoImage(file='line.png')
+line_label2 = Label(templete,bg='#3D2B27',border=0,image=line_photo2)
+line_label2.place(x=0,y=400)
+
+title_label1 = Label(templete,text="Label",foreground='white',font=('bold',13),bg='#3D2B27')
+title_label1.place(x=130,y=375)
+
+comingsoon = Label(templete,text="Coming\nSoon",foreground='white',font=('bold',40),background='#3D2B27')
+comingsoon.place(x=60,y=460)
+
+templete.mainloop()
